14545fe32aa4ffde0a88793227ca5cda1355b399
krys <mentally.gamez.soft@gmail.com>
2024-02-04 15:37:49 +0000

    display message indicating fronm what environment test is executed

--- tests/test_load_env_variables.py
+++ tests/test_load_env_variables.py
@@ -8,9 +8,11 @@ class TestLoadEnvVariables(unittest.TestCase):
 
     def test_load_env_for_local_dev(self):
         if not env_loader.is_application_executed_on_cicd_runner():
+            print("test dev local executed")
             self.assertEqual(env_loader.get("APP_ENV"), "local.dev","The local dev APP_ENV variable is not set")
 
     def test_load_env_for_cicd(self):
         if env_loader.is_application_executed_on_cicd_runner():
+            print("test ci_cd executed")
             self.assertEqual(env_loader.get("APP_ENV"), "cicd_runner","The local dev APP_ENV variable is not set")
 
